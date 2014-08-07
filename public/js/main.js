71ab097d880302e47dbb8b78fa2b4de553ea7584
Simon Sinclair <simon@sinclair>
2014-08-07 17:15:37 +0100

    Add interactivity to required elements

--- public/js/main.js
+++ public/js/main.js
@@ -22,14 +22,46 @@
 			imageWidth: 976
 		},
 
+		isCaptionsVisible: false,
+		isThumbsVisible: true,
+
 		init: function(id) {
 			Gallery.$elem = $(id);
+			Gallery.bindEvents();
 			Gallery.setImagesWidth();
+		},
+
+		bindEvents: function() {
+			$('#js-gallery-previous').on('click', Gallery.previousImage);
+			$('#js-gallery-next').on('click', Gallery.nextImage);
+			$('#js-toggle-thumbs').on('click', Gallery.toggleThumbs);
+			$('#js-toggle-caption').on('click', Gallery.toggleCaption);
+			$('#js-gallery-thumbs').on('click', 'li', Gallery.navigateToThumb);
 		},
 
 		setImagesWidth: function() {
 			var imagesWidth = Gallery.config.imageWidth * Gallery.config.numImages;
 			$('#js-gallery-images', Gallery.$elem).css('width', imagesWidth);
+		},
+
+		previousImage: function(e) {
+			console.log(this);
+		},
+
+		nextImage: function(e) {
+			console.log(this);
+		},
+
+		toggleThumbs: function(e) {
+			console.log(this);
+		},
+
+		toggleCaption: function(e) {
+			console.log(this);
+		},
+
+		navigateToThumb: function(e) {
+			console.log(this);
 		}
 
 	};
